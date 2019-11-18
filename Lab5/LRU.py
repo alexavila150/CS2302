672dd87f29ea937acc866bb4fb22fbed691431fe
alexavila150 <alexadrianavila@gmail.com>
2019-11-17 19:10:30 -0700

    LRU get and put methods finished

--- Lab5/LRU.py
+++ Lab5/LRU.py
@@ -1,26 +1,39 @@
 class LRU(object):
     def __init__(self, capacity):
-        self.head = Node()
-        self.tail = Node()
+        self.head = Node(0)
+        self.tail = Node(0)
         self.capacity = capacity
         self.size = 0
         self.node_of_key = dict()
+        self.keys = set()
         self.head.next = self.tail
         self.tail.prev = self.head
 
     def get(self, key):
         # get node that has the search key
         node = self.node_of_key[key]
-
         # update node as most recent node
-
-        return node.item
+        self.send_node_to_head(key, node)
+        return node.value
 
     def put(self, key, value):
+        # if key is in LRU then update the value
+        if key in self.keys:
+            node = self.node_of_key[key]
+            node.value = value
+            return
+
         if self.is_full():
             self.remove_last_node()
 
-        self.send_node_to_head(key, value)
+        node = Node(key, value)
+        self.insert_node_first(key, node)
+
+    def size(self):
+        return self.size
+
+    def max_capacity(self):
+        return self.capacity
 
     def send_node_to_head(self, key, node):
         self.remove_node(node)
@@ -35,7 +48,9 @@ class LRU(object):
 
     def remove_last_node(self):
         # remove node from dictionary
-        self.node_of_key.pop(self.tail.prev, None)
+        last_key = self.tail.prev.key
+        self.keys.remove(last_key)
+        self.node_of_key.pop(self.tail.prev.key, None)
 
         # remove from linked list
         next_node = self.tail
@@ -44,19 +59,21 @@ class LRU(object):
         prev_node.next = next_node
 
         # change size of linked list
-        self.size += 1
+        self.size -= 1
 
     # inserts node to linked list and dict
     def insert_node_first(self, key, node):
         # insert node to dictionary
+        self.keys.add(key)
         self.node_of_key[key] = node
 
         # insert node to linked list
-        target_node = self.head.next
-        prev_node = target_node.prev
-        next_node = target_node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
+        prev_node = self.head
+        next_node = prev_node.next
+        prev_node.next = node
+        node.prev = prev_node
+        next_node.prev = node
+        node.next = next_node
 
         # increment size
         self.size += 1
@@ -64,12 +81,32 @@ class LRU(object):
     def is_full(self):
         return self.capacity == self.size
 
-    def print(self):
-        return
+    def __str__(self):
+        result = ""
+        curr = self.head.next
+        while curr is not self.tail:
+            result += str(curr.value) + " "
+            curr = curr.next
+        return result
 
 
 class Node(object):
-    def __init__(self, value=None):
+    def __init__(self, key, value=None):
+        self.key = key
         self.value = value
         self.next = None
         self.prev = None
+
+
+my_lru = LRU(5)
+my_lru.put("Alex", 19)
+my_lru.put("Turin", 29)
+my_lru.put("Arturo", 57)
+my_lru.put("Irvin", 18)
+my_lru.put("Ivonne", 15)
+my_lru.put("Ivonne", 15)
+my_lru.put("Karen", 14)
+my_lru.put("Cynthia", 23)
+my_lru.put("Carlos", 15)
+my_lru.get("Cynthia")
+print(my_lru)
