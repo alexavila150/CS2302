2ee5894a3a7f2c3247548bb7ee40b36d1a949ea4
alexavila150 <alexadrianavila@gmail.com>
2020-02-27 18:48:07 -0700

    HI

--- ComputerVision/CV.py
+++ ComputerVision/CV.py
@@ -1,16 +1,18 @@
 import cv2
 
+
 def convertToGray(image):
-    #for i in range(image.shape[0]):
-     #   for j in range(image.shape[1]):
-            #avg = (.3 * image[i][j][0] + .3 * image[i][j][1] + .3 * image[i][j][2])
-            #image[i][j][0] = avg
-            #image[i][j][1] = avg
-            #image[i][j][2] = avg
-      #      image[i][j][0] = 255 - image[i][j][0]
-       #     image[i][j][1] = 255 - image[i][j][1]
-        #    image[i][j][2] = 255 - image[i][j][2]
+    # for i in range(image.shape[0]):
+    #   for j in range(image.shape[1]):
+    # avg = (.3 * image[i][j][0] + .3 * image[i][j][1] + .3 * image[i][j][2])
+    # image[i][j][0] = avg
+    # image[i][j][1] = avg
+    # image[i][j][2] = avg
+    #      image[i][j][0] = 255 - image[i][j][0]
+    #     image[i][j][1] = 255 - image[i][j][1]
+    #    image[i][j][2] = 255 - image[i][j][2]
     return 255 - image
+
 
 # Get first camera that it finds open
 cap = cv2.VideoCapture(0)
@@ -21,10 +23,10 @@ while True:
 
     # Operation to do on the frame
     frame = convertToGray(frame)
-# cv2 implementation of it -> gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2 implementation of it -> gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
